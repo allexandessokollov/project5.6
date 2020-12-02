cae2b9e522cb1f34d5fbb7a216c47484a17601c9
allexandessokollov <allexandersokollov@gmail.com>
2020-12-02 16:00:30 +0300

    finally create project and added searching functions

--- project5_6.cpp
+++ project5_6.cpp
@@ -3,6 +3,8 @@
 #include <vcl.h>
 #pragma hdrstop
 //---------------------------------------------------------------------------
+USEFORM("..\fromPage43\fromPage43\fromPage43.cpp", Form1);
+//---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
         try
